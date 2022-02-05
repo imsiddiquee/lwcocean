1f75d4a72f0b83fb0625ca30581ac0a3bc1b7715
Shamim <imsiddiquee@gmail.com>
2022-02-05 20:37:11 +0600

    update server pagination with wire adapter

--- force-app/main/default/lwc/accountPaginationList/accountPaginationList.js
+++ force-app/main/default/lwc/accountPaginationList/accountPaginationList.js
@@ -1,4 +1,5 @@
-import { LightningElement, track } from "lwc";
+import { LightningElement, track, wire } from "lwc";
+import { refreshApex } from "@salesforce/apex";
 import getAccountList from "@salesforce/apex/AccountPaginationController.getAccountList";
 
 export default class AccountPaginationList extends LightningElement {
@@ -14,52 +15,84 @@ export default class AccountPaginationList extends LightningElement {
     @track isNext = true;
     @track accounts = [];
 
+    pageListResponse;
+
     //On load
     connectedCallback() {
-        this.getAccounts();
+        //this.getAccounts();
     }
 
     //handle next
     handleNext() {
+        this.loader = true;
         this.pageNumber = this.pageNumber + 1;
-        this.getAccounts();
+        refreshApex(this.pageListResponse).finally(() => (this.processing = false));
     }
 
     //handle prev
     handlePrev() {
+        this.loader = true;
         this.pageNumber = this.pageNumber - 1;
-        this.getAccounts();
+        refreshApex(this.pageListResponse).finally(() => (this.processing = false));
+    }
+
+    @wire(getAccountList, { pageSize: "$pageSize", pageNumber: "$pageNumber" })
+    wiredGetAccountList(response) {
+        this.pageListResponse = response;
+
+        let data = response.data;
+        let error = response.error;
+
+        if (data || error) {
+            //this.processing = false;
+            this.loader = true;
+        }
+
+        if (data) {
+            this.loader = false;
+            let resultData = JSON.parse(data);
+            this.accounts = resultData.accounts;
+            this.pageNumber = resultData.pageNumber;
+            this.totalRecords = resultData.totalRecords;
+            this.recordStart = resultData.recordStart;
+            this.recordEnd = resultData.recordEnd;
+            this.totalPages = Math.ceil(resultData.totalRecords / this.pageSize);
+            this.isNext = this.pageNumber === this.totalPages || this.totalPages === 0;
+            this.isPrev = this.pageNumber === 1 || this.totalRecords < this.pageSize;
+        } else if (error) {
+            console.log("error");
+            this.loader = false;
+        }
     }
 
     //get accounts
-    getAccounts() {
-        this.loader = true;
-        getAccountList({ pageSize: this.pageSize, pageNumber: this.pageNumber })
-            .then((result) => {
-                this.loader = false;
-                if (result) {
-                    var resultData = JSON.parse(result);
-                    this.accounts = resultData.accounts;
-                    this.pageNumber = resultData.pageNumber;
-                    this.totalRecords = resultData.totalRecords;
-                    this.recordStart = resultData.recordStart;
-                    this.recordEnd = resultData.recordEnd;
-                    this.totalPages = Math.ceil(resultData.totalRecords / this.pageSize);
-                    this.isNext = this.pageNumber == this.totalPages || this.totalPages == 0;
-                    this.isPrev = this.pageNumber == 1 || this.totalRecords < this.pageSize;
-                }
-            })
-            .catch((error) => {
-                this.loader = false;
-                this.error = error;
-            });
-    }
+    // getAccounts() {
+    //     getAccountList({ pageSize: pageSize, pageNumber: this.pageNumber })
+    //         .then((result) => {
+    //             this.loader = false;
+    //             if (result) {
+    //                 var resultData = JSON.parse(result);
+    //                 this.accounts = resultData.accounts;
+    //                 this.pageNumber = resultData.pageNumber;
+    //                 this.totalRecords = resultData.totalRecords;
+    //                 this.recordStart = resultData.recordStart;
+    //                 this.recordEnd = resultData.recordEnd;
+    //                 this.totalPages = Math.ceil(resultData.totalRecords / this.pageSize);
+    //                 this.isNext = this.pageNumber == this.totalPages || this.totalPages == 0;
+    //                 this.isPrev = this.pageNumber == 1 || this.totalRecords < this.pageSize;
+    //             }
+    //         })
+    //         .catch((error) => {
+    //             this.loader = false;
+    //             this.error = error;
+    //         });
+    // }
 
     //display no records
     get isDisplayNoRecords() {
         var isDisplay = true;
         if (this.accounts) {
-            if (this.accounts.length == 0) {
+            if (this.accounts.length === 0) {
                 isDisplay = true;
             } else {
                 isDisplay = false;
