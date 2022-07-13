00734d3ce1bba3c14a444ef0bf32dcdc31a0dddc
Shamim <imsiddiquee@gmail.com>
2022-07-13 22:41:59 +0600

    update

--- force-app/main/default/lwc/myProjectContainer/myProjectContainer.js
+++ force-app/main/default/lwc/myProjectContainer/myProjectContainer.js
@@ -1,6 +1,27 @@
 import { LightningElement } from "lwc";
 import * as PROJECT_DATA from "./myProjectContainerData";
 
+/**
+ * dependent child component
+ * 
+ * lwc component 
+ * 
+ * lightningLoader
+ * myProjectHeader
+ * myProjectSummary
+ * myProjectPortfolio
+ * myProjectPortfolioDescription
+ * 
+ * aura component
+ *  
+ * BookingFormApp
+ * 
+ * visualforce page
+ * 
+ * myProjectPreview
+ * 
+ 
+ */
 export default class MyProjectContainer extends LightningElement {
     HEADER_DETAILS = PROJECT_DATA.HEADER_DETAILS;
     PROJECT_SUMMARY = PROJECT_DATA.PROJECT_SUMMARY;
--- force-app/main/default/lwc/myProjectPortfolio/myProjectPortfolio.js
+++ force-app/main/default/lwc/myProjectPortfolio/myProjectPortfolio.js
@@ -3,18 +3,14 @@ import { LightningElement, track, api } from "lwc";
 export default class MyProjectPortfolio extends LightningElement {
     @api details;
 
+    isLoading = false;
+
     showModalBox(event) {
-        //console.log("event-->", this.details[0].id);
-        // console.log("event-->", event.currentTarget.dataset.id);
-        // console.log("event-->", event.currentTarget.dataset.item.id);
-        // console.log("event-->", JSON.parse(event.currentTarget.dataset.item));
-        // console.log("event-->", event.target.dataset);
+        this.isLoading = true;
 
         let selectedItem = this.details.find((p) => p.label === event.currentTarget.dataset.item);
 
-        //console.log("show modal box", selectedItem);
-        //this.isShowModal = true;
-
+        this.isLoading = false;
         this.template.querySelector("c-my-project-portfolio-description").details = selectedItem;
         this.template.querySelector("c-my-project-portfolio-description").showModal();
     }
